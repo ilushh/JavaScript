d03ace0d767394fc4fe2b175ce46ecf05c12c02a
Juelide <julide_c@hotmail.com>
2022-11-22 23:55:29 +0100

    load

---
+++ Week_2/Udemy 1/Liqui Planer/JS/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let titel_1 = prompt("Titel:");
+console.log(titel_1);
+
+let type_1 = prompt("Type (Einnahmen oder Ausgaben):");
+console.log(type_1);
+
+let datum = prompt("Datum (jjjj-mm-tt:");
+console.log(datum);
+
+//Bilanzen
+let einnahmen;
+let ausgaben;
+let bilanz;
+
+// ZEilenumruch
+
+let zeilenumbruch = "asdfghj" +
+"\qwertz"+
+"\t\t\ hier steht eine Einrückung";
---
+++ Week_2/Udemy 1/udemy1.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*let titel_1 = prompt("Titel:");
+console.log(titel_1);
+
+let type_1 = prompt("Type (Einnahmen oder Ausgaben):");
+console.log(type_1);
+
+let datum = prompt("Datum (jjjj-mm-tt:");
+console.log(datum);*/
+
+
+// ZEilenumruch
+
+let zeilenumbruch = "asdfghj" +
+"\nqwertz" +
+"\n\t\ hier steht eine Einrückung";
+console.log(zeilenumbruch);
